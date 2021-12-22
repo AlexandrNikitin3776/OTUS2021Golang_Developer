f858f18a6336fcf7413f035e7e76d3760781afa4
Александр Никитин <anikitin@ubic.tech>
2021-12-22 23:10:58 +0300

    Refactor code: make simple functions. Add unit tests

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -6,45 +6,58 @@ import (
 	"strings"
 )
 
-type WordCount struct {
-	text  string
-	count int
-}
-
 const topWordsCount = 10
 
-func Top10(text string) []string {
-	re := regexp.MustCompile(`\p{L}+-\p{L}+|\p{L}+`)
-	words := re.FindAllString(text, -1)
+var regexpWordPattern = regexp.MustCompile(`[\p{L}\d]+[\p{L}\d-]+[\p{L}\d]+|[\p{L}\d]+`)
 
+func getWordsFromText(text string) []string {
+	return regexpWordPattern.FindAllString(text, -1)
+}
+
+func makeFrequencyMap(words []string) map[string]int {
+	if len(words) == 0 {
+		return nil
+	}
 	frequencyMap := make(map[string]int)
 	for _, word := range words {
 		frequencyMap[strings.ToLower(word)]++
 	}
+	return frequencyMap
+}
 
-	wordSlice := make([]WordCount, 0, len(words))
-	for word, count := range frequencyMap {
-		wordSlice = append(wordSlice, WordCount{word, count})
+func getKeysFromMap(inputMap map[string]int) []string {
+	if len(inputMap) == 0 {
+		return nil
 	}
+	keys := make([]string, 0, len(inputMap))
+	for key := range inputMap {
+		keys = append(keys, key)
+	}
+	return keys
+}
 
-	sort.Slice(wordSlice, func(i, j int) bool {
-		if wordSlice[i].count == wordSlice[j].count {
-			return wordSlice[i].text < wordSlice[j].text
+func minOfTwo(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func sortSliceByMapValues(sl []string, mp map[string]int) {
+	sort.Slice(sl, func(i, j int) bool {
+		iSliceValue := mp[sl[i]]
+		jSliceValue := mp[sl[j]]
+		if iSliceValue == jSliceValue {
+			return sl[i] < sl[j]
 		}
-		return wordSlice[i].count > wordSlice[j].count
+		return iSliceValue > jSliceValue
 	})
+}
 
-	result := make([]string, 0, 10)
-	var upperIndex int
-
-	if len(wordSlice) >= topWordsCount {
-		upperIndex = topWordsCount
-	} else {
-		upperIndex = len(wordSlice)
-	}
-
-	for _, word := range wordSlice[:upperIndex] {
-		result = append(result, word.text)
-	}
-	return result
+func Top10(text string) []string {
+	words := getWordsFromText(text)
+	frequencyMap := makeFrequencyMap(words)
+	uniqueWords := getKeysFromMap(frequencyMap)
+	sortSliceByMapValues(uniqueWords, frequencyMap)
+	return uniqueWords[:minOfTwo(len(uniqueWords), topWordsCount)]
 }
