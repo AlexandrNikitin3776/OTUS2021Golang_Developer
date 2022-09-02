d71c4d0656aad5799f5396e7b742a754dcf49eb3
Александр Никитин <anikitin@ubic.tech>
2022-09-02 09:58:09 +0300

    Implement base copy functional

--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"flag"
+	"io"
+	"log"
+	"os"
 )
 
 var (
@@ -22,5 +25,32 @@ func main() {
 }
 
 func dd(from, to string, limit, offset int64) {
+	fromFile, err := os.Open(from)
+	if err != nil {
+		log.Fatalf("Error while opening %v: %v", from, err)
+	}
+
+	defer func() {
+		if err := fromFile.Close(); err != nil {
+			log.Fatalf("Error while closing file %v: %v", from, err)
+		}
+	}()
+
+	toFile, err := os.Create(to)
+	if err != nil {
+		log.Fatalf("Error while opening %v: %v", to, err)
+	}
+
+	defer func() {
+		if err := toFile.Close(); err != nil {
+			log.Fatalf("Error while closing file %v: %v", to, err)
+		}
+	}()
+
+	if written, err := io.Copy(toFile, fromFile); err != nil {
+		log.Fatalf("Error while copying from %v to %v: %v", from, to, err)
+	} else {
+		log.Printf("Copied %v bytes", written)
+	}
 
 }
