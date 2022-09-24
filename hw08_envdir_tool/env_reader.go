f0fda179aa4f0cb425e47a8739dadf81ca675d2a
Александр Никитин <anikitin@ubic.tech>
2022-09-25 00:34:01 +0300

    Implement read dir function

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +17,63 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	environment := make(map[string]EnvValue)
+
+	for _, file := range files {
+		if !isValidFileName(file.Name()) {
+			continue
+		}
+
+		filePath := filepath.Join(dir, file.Name())
+		content, err := readFileString(filePath)
+		if err != nil {
+			return nil, err
+		}
+
+		environment[file.Name()] = EnvValue{
+			convertContent(content),
+			isFileEmpty(content),
+		}
+	}
+	return environment, nil
+}
+
+func convertContent(content string) string {
+	content = trimSpacesAndTabsRight(content)
+	content = replaceHexZeroesToNewLines(content)
+	return content
+}
+
+func isValidFileName(fileName string) bool {
+	if strings.Contains(fileName, "=") {
+		return false
+	}
+	return true
+}
+
+func readFileString(path string) (string, error) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(content), nil
+}
+
+func isFileEmpty(content string) bool {
+	if len(content) == 0 {
+		return true
+	}
+	return false
+}
+
+func trimSpacesAndTabsRight(content string) string {
+	return strings.TrimRight(content, " \t")
+}
+
+func replaceHexZeroesToNewLines(content string) string {
+	return strings.Replace(content, "\x00", "\n", -1)
 }
--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,116 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+type EnvFile struct {
+	name    string
+	content string
+}
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	testCases := []struct {
+		name     string
+		envFiles []EnvFile
+		expected Environment
+	}{
+		{
+			"empty",
+			nil,
+			make(Environment)},
+		{
+			"env to replace",
+			[]EnvFile{{"FOO", "19"}},
+			Environment{"FOO": EnvValue{"19", false}},
+		},
+		{
+			"env to remove",
+			[]EnvFile{{"BAR", ""}},
+			Environment{"BAR": EnvValue{"", true}},
+		},
+		{
+			"with spaces in the end",
+			[]EnvFile{{"FOO", "19     "}},
+			Environment{"FOO": EnvValue{"19", false}},
+		},
+		{
+			"with tabs in the end",
+			[]EnvFile{{"FOO", "19\t\t\t\t\t"}},
+			Environment{"FOO": EnvValue{"19", false}},
+		},
+		{
+			"replace terminal zeroes to \n",
+			[]EnvFile{{"FOO", "19\x0042"}},
+			Environment{"FOO": EnvValue{"19\n42", false}},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tempDir, err := os.MkdirTemp(os.TempDir(), tc.name)
+			if err != nil {
+				t.Fatal("Error while creating temporary directory")
+			}
+
+			defer func() {
+				if err := os.RemoveAll(tempDir); err != nil {
+					t.Fatalf("Error while removing directory %v: %v", tempDir, err)
+				}
+			}()
+
+			for _, envFile := range tc.envFiles {
+				filePath := filepath.Join(tempDir, envFile.name)
+				if err = os.WriteFile(filePath, []byte(envFile.content), os.ModePerm); err != nil {
+					t.Fatalf("Can't create temporary file: %v", err)
+				}
+			}
+
+			result, err := ReadDir(tempDir)
+			t.Log(result, err)
+			require.NoError(t, err, "shouldn't be any error")
+			require.Equal(t, tc.expected, result)
+		})
+	}
+
+}
+
+func TestIsValidFileName(t *testing.T) {
+	tests := []struct {
+		name     string
+		fileName string
+		want     bool
+	}{
+		{"valid filename", "FOO", true},
+		{"digit filename", "3", true},
+		{"invalid filename", "FO0=123", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isValidFileName(tt.fileName); got != tt.want {
+				t.Errorf("isValidFileName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsFileEmpty(t *testing.T) {
+	tests := []struct {
+		name    string
+		content string
+		want    bool
+	}{
+		{"empty", "", true},
+		{"not empty", "not empty content", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isFileEmpty(tt.content); got != tt.want {
+				t.Errorf("isFileEmpty() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
